3cf04af8e12b893ac38b3658ccbf3491881be440
sthg-anon <sthg.anon@gmail.com>
2025-11-28 14:07:40 -0800

    Test image is drawn to window.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <rlImGui.h>
 #include <imgui.h>
 
+#include "assets/sprites/test_image.hpp"
+
 int main()
 {
     const int screenWidth = 800;
@@ -17,6 +19,12 @@ int main()
     rlImGuiSetup(true);
     auto rlImGuiCleanup = gsl::finally(rlImGuiShutdown);
 
+    Image test_image = LoadImageFromMemory(".png", sfa::test_image, sfa::test_image_size);
+    auto unloadImage = gsl::finally([&test_image]() { UnloadImage(test_image); });
+
+    Texture2D test_texture = LoadTextureFromImage(test_image);
+    auto unloadTexture = gsl::finally([&test_texture]() { UnloadTexture(test_texture); });
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
@@ -25,10 +33,12 @@ int main()
         rlImGuiBegin();
         auto rlImGuiEndGuard = gsl::finally(rlImGuiEnd);
 
-        ImGui::ShowDemoWindow();
+        //ImGui::ShowDemoWindow();
 
         ClearBackground(RAYWHITE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        DrawTexture(test_texture, screenWidth / 2 - test_texture.width / 2, screenHeight / 2 - test_texture.height / 2, WHITE);
     }
 
     return EXIT_SUCCESS;
